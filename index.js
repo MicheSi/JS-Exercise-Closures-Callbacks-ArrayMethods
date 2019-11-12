3c8254d6afcf007f9816f41fe28e1bc2d5b1379d
mlazzell17@gmail.com <MicheSi>
2019-11-12 09:58:21 -0700

    completed first higher order array challenge

--- index.js
+++ index.js
@@ -184,8 +184,12 @@ function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners) {
+ let fullNames = [];
+ runners.forEach((curVal) => {
+   return fullNames.push(`${curVal.last_name}, ${curVal.first_name}`);
+ });
+ return fullNames;
 }
 
 /**
